a3152168ffad469f5550face3eeb48f96954aef6
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-02-15 23:47:29 -0600

    add seed.sql file

---
+++ bamazonCustomer.js
@@ -0,0 +1,6 @@
+// first display all of the items available for sale. Include the ids, names, and prices of products for sale.
+// The app should then prompt users with two messages.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
+// check if your store has enough of the product to meet the customer's request.
+// If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
