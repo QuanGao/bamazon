bc7e3341b53004baa72a09c7341e8a2a8294a151
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-02-18 13:14:45 -0600

    add stock, view low/full inventory functions working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,79 +1,85 @@
-
 const inq = require("inquirer");
 const mysql = require("mysql");
 const Table = require("tty-table");
 let numItems;
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "yllen",
-    database: "bamazon"
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "",
+  database: "bamazon"
 });
-  
-connection.connect(function(err) {
+
+connection.connect(function (err) {
   if (err) throw err;
   console.log(`
   Welcome to the bamazon store!`);
   start();
 });
 
-function Customer (query_id, query_num) {
+function Customer(query_id, query_num) {
   this.query_id = query_id;
   this.query_num = query_num;
 }
 
-function displayTable (res) {
-  let header = [{value:"item_id", width : 10}, {value:"product_name"}, {value:"price"}]
-  let productTable = Table(header,res);
+function displayTable(res) {
+  let header = [{
+    value: "item_id",
+    width: 10
+  }, {
+    value: "product_name"
+  }, {
+    value: "price"
+  }]
+  let productTable = Table(header, res);
   console.log(productTable.render());
   numItems = res.length;
 }
 
-function start () {
+function start() {
   console.log(`
   Here are our products for sale:
   `);
-  connection.query(`SELECT item_id, product_name, price FROM products`, function(err, res) {
+  connection.query(`SELECT item_id, product_name, price FROM products`, function (err, res) {
     if (err) throw err;
-    displayTable (res)
-    askCustomer(); 
+    displayTable(res)
+    askCustomer();
   })
 }
 
-function askCustomer(){
-  inq.prompt([
-    {
-      name:"id",
-      type:"input",
+function askCustomer() {
+  inq.prompt([{
+      name: "id",
+      type: "input",
       message: `
   What product would you like to purchase (enter item_id)?`,
-      validate: function validateGuess(id) {
+      validate: function (id) {
         let float = parseFloat(id)
         return Number.isInteger(float) && float > 0 && float <= numItems;
       }
     },
     {
-      name:"quantity",
-      type:"input",
+      name: "quantity",
+      type: "input",
       message: `
   How many?`,
-      validate: function validateGuess(num) {
-        return Number.isInteger(parseFloat(num)) && parseFloat(num) >0;
+      validate: function(num) {
+        return Number.isInteger(parseFloat(num)) && parseFloat(num) > 0;
       }
     },
   ]).then(ans => {
-    let customer = new Customer (ans.id, ans.quantity)
+    let customer = new Customer(ans.id, ans.quantity)
     checkStock(customer);
   })
 }
-function checkStock (customer) {
-    connection.query(`SELECT price, stock_quantity FROM products WHERE item_id=${customer.query_id}`, function(err, res) {
+
+function checkStock(customer) {
+  connection.query(`SELECT price, stock_quantity FROM products WHERE item_id=${customer.query_id}`, function (err, res) {
     if (err) throw err;
     let price = res[0].price;
     let quant = res[0].stock_quantity;
-    if (customer.query_num > quant){
+    if (customer.query_num > quant) {
       console.log(`
   insufficient quantity
       `);
@@ -86,25 +92,21 @@ function checkStock (customer) {
   })
 }
 
-function updateStock (itemID, newQuantity, sum){
+function updateStock(itemID, newQuantity, sum) {
   connection.query(
-    "UPDATE products SET ? WHERE ?",
-    [
-      {
-        stock_quantity:newQuantity
+  "UPDATE products SET ? WHERE ?", [{
+        stock_quantity: newQuantity
       },
       {
         item_id: itemID
       }
-    ],function(err, res){
-      if(err) throw err;
+    ],
+    function (err, res) {
+      if (err) throw err;
       console.log(`
   Your total is ${sum}
-      `);
+        `);
       connection.end();
     }
   )
 }
-
-
-
---
+++ bamazonManager.js
@@ -0,0 +1,166 @@
+//   * List a set of menu options:View Products for Sale;View Low Inventory;Add to Inventory ;Add New Product
+//   * If a manager selects `View Products for Sale`, the app should list every available item: the item IDs, names, prices, and quantities.
+//   * If a manager selects `View Low Inventory`, then it should list all items with an inventory count lower than five.
+//   * If a manager selects `Add to Inventory`, your app should display a prompt that will let the manager "add more" of any item currently in the store.
+//   * If a manager selects `Add New Product`, it should allow the manager to add a completely new product to the store.
+const inq = require("inquirer");
+const mysql = require("mysql");
+const Table = require("tty-table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "bamazon"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log(`connected!`);
+    // chooseTask();
+    // readInventory()
+    readInventory(addStock)
+
+
+});
+
+let chooseTask = function () {
+    inq.prompt([{
+        name: "task",
+        type: "list",
+        message: "What would you like to do boss?",
+        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Done for now"]
+
+    }]).then(function (ans) {
+        switch (ans.task) {
+            case "View Products for Sale":
+                viewInventory();
+                break;
+            case "View Low Inventory":
+                findLowStock();
+                break;
+            case "Add to Inventory":
+                addStock();
+                break;
+            case "Add New Product":
+                addNewProduct();
+                break;
+            case "Done for now":
+                connection.end();
+                break;
+            default:
+                return;
+        }
+    })
+}
+
+
+
+let displayTable = function (res) {
+    let header = [{
+        value: "item_id",
+        width: 10
+    }, {
+        value: "product_name"
+    }, {
+        value: "price"
+    }, {
+        value: "stock_quantity"
+    }]
+    let productTable = Table(header, res);
+    console.log(productTable.render());
+}
+
+// let viewInventory = function () {
+//     connection.query(`SELECT * FROM products`, function (err, res) {
+//         if (err) throw err;
+//         console.log(`
+//         Here's the full inventory:
+//         `);
+//         displayTable(res)
+//     })
+// }
+
+let displayInventory = function (res) {
+    console.log(`
+                        Here's the full inventory:`);
+    displayTable(res)
+}
+
+let readInventory = function (cb) {
+    connection.query(`SELECT * FROM products`, function (err, res) {
+        if (err) throw err;
+        cb(res);
+    })
+}
+
+let addStock = function (res) {
+    let products = res.map(item => {
+        return `${item.item_id}:${item.product_name}`
+    })
+    inq.prompt([{
+            name: "stockUp",
+            message: "What product would you like to stock more?",
+            type: "list",
+            choices: products
+        },
+        {
+            name: "quant",
+            message: "How many would like to add?",
+            type: "input",
+            validate: function (num) {
+                return Number.isInteger(parseFloat(num)) && parseFloat(num) > 0;
+            }
+        }
+
+    ]).then(function (ans) {
+        let id_stockUp = ans.stockUp.split(":")[0];
+        let num_stockUp = ans.quant;
+        console.log(id_stockUp, num_stockUp);
+        updateQuant(id_stockUp, num_stockUp);
+
+
+
+    })
+}
+
+let findLowStock = function () {
+    console.log(`
+    Here are items that have less than 5 left in the inventory:':
+    `);
+    connection.query(`SELECT * FROM products WHERE stock_quantity < 100`, function (err, res) {
+        if (err) throw err;
+        displayTable(res)
+    })
+}
+
+let updateQuant = function (id, num) {
+        var query = connection.query(
+            `UPDATE products SET stock_quantity = stock_quantity+${num} WHERE ?`, [
+                {
+                    item_id: id
+                }
+            ],
+            function (err, res) {
+                readInventory(displayInventory)
+            })
+        }
+
+
+let addNewProduct = function () {}
+
+
+
+
+        // function Manager() {
+        //     this.viewInventory= function () {
+        //     };
+        //     this.findLowStock = function () {};
+        //     this.addStock = function () {
+
+        //     };
+        //     this.addNewProduct = function () {
+
+        //     }
+        // }
